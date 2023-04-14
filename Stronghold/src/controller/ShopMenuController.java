459f941587d7ccf826c89371efbe3bb344a8a344
Mehrshad Dehghani <Dehghani.Mehrshad82@gmail.com>
2023-04-15 02:23:40 +0330

    almost finished

--- Stronghold/src/controller/ShopMenuController.java
+++ Stronghold/src/controller/ShopMenuController.java
@@ -1,4 +1,15 @@
 package controller;
 
+import java.util.regex.Matcher;
+
 public class ShopMenuController {
+    public String showPriceList(Matcher matcher){
+        return null;
+    }
+    public String buy(Matcher matcher){
+        return null;
+    }
+    public String sell(Matcher matcher){
+        return null;
+    }
 }
