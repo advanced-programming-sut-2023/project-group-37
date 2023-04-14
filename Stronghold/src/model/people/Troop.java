e3f0925c8d246d9d897eb1e62d1c1571d64a8583
Payea <mmdPayea@gmail.com>
2023-04-14 23:50:46 +0430

    currentBuilding, currentTroop, Tile, Texture, Engineer, Tunneler

--- Stronghold/src/model/people/Troop.java
+++ Stronghold/src/model/people/Troop.java
@@ -1,6 +1,7 @@
 package model.people;
 
 import model.Government;
+import model.Tile;
 import model.enums.Weapons;
 import model.people.enums.Troops;
 
@@ -9,9 +10,23 @@ import java.util.ArrayList;
 public class Troop extends Person {
 
     private final ArrayList<Weapons> weapons;
+    private static ArrayList<Troop> unit = new ArrayList<>();
+
 
     public Troop(Government government, Troops troop) {
         super(government, troop.getSpeed(), troop.getMaxHitPoints());
         this.weapons = troop.getWeapons();
+    }
+
+    public void setUnit(ArrayList<Troop> unit) {
+        Troop.unit = unit;
+    }
+
+    public ArrayList<Troop> getUnit() {
+        return unit;
+    }
+
+    public static void airAttack(Tile tile) {
+
     }
 }
