ca09cdd93fd1ddf86decd419cbfb0a758b3c24c7
Bardia <itsBardiaMandegar@gmail.com>
2023-06-09 13:17:55 +0330

    buildings image added!

--- src/main/java/model/buildings/DefensiveBuildingType.java
+++ src/main/java/model/buildings/DefensiveBuildingType.java
@@ -2,18 +2,37 @@ package model.buildings;
 
 import javafx.scene.image.Image;
 
+import java.util.Objects;
+
 public enum DefensiveBuildingType {
 
-    KEEP("", 1, 0, 0, 16),
-    WALL("wall", 250, 1, 1.1, 1),
-    STAIRS("stairs", 150, 1, 0, 1),
-    SMALL_GATEHOUSE("small gatehouse", 350, 10, 1.2, 8),
-    LARGE_GATEHOUSE("large gatehouse", 450, 20, 1.4, 12),
-    LOOKOUT_TOWER("lookout tower", 250, 10, 2.2, 4),
-    PERIMETER_TOWER("perimeter tower", 350, 10, 1.6, 6),
-    DEFENCE_TOWER("defence tower", 350, 15, 1.6, 6),
-    SQUARE_TOWER("square tower", 450, 35, 2, 10),
-    ROUND_TOWER("round tower", 450, 40, 1.8, 10),
+    KEEP("", new Image(Objects.requireNonNull(DefensiveBuildingType.class.getResource
+            ("/Image/building/keep.png")).toExternalForm()), 1, 0, 0, 16),
+    WALL("wall", new Image(Objects.requireNonNull(DefensiveBuildingType.class.getResource
+            ("/Image/building/wall.png")).toExternalForm()), 250, 1, 1.1, 1),
+    STAIRS("stairs", new Image(Objects.requireNonNull(DefensiveBuildingType.class.getResource
+            ("/Image/building/stairs.png")).toExternalForm()), 150, 1, 0, 1),
+    SMALL_GATEHOUSE("small gatehouse", new Image(Objects.requireNonNull(DefensiveBuildingType.class.getResource
+            ("/Image/building/small-gatehouse.png")).toExternalForm()), 350, 10, 1.2,
+            8),
+    LARGE_GATEHOUSE("large gatehouse", new Image(Objects.requireNonNull(DefensiveBuildingType.class.getResource
+            ("/Image/building/large-gatehouse.png")).toExternalForm()), 450, 20, 1.4,
+            12),
+    LOOKOUT_TOWER("lookout tower", new Image(Objects.requireNonNull(DefensiveBuildingType.class.getResource
+            ("/Image/building/lookout-tower.png")).toExternalForm()), 250, 10, 2.2,
+            4),
+    PERIMETER_TOWER("perimeter tower", new Image(Objects.requireNonNull(DefensiveBuildingType.class.getResource
+            ("/Image/building/perimeter-tower.png")).toExternalForm()), 350, 10, 1.6,
+            6),
+    DEFENCE_TOWER("defence tower", new Image(Objects.requireNonNull(DefensiveBuildingType.class.getResource
+            ("/Image/building/defence=tower.png")).toExternalForm()), 350, 15, 1.6,
+            6),
+    SQUARE_TOWER("square tower", new Image(Objects.requireNonNull(DefensiveBuildingType.class.getResource
+            ("/Image/building/square-tower.png")).toExternalForm()), 450, 35, 2,
+            10),
+    ROUND_TOWER("round tower", new Image(Objects.requireNonNull(DefensiveBuildingType.class.getResource
+            ("/Image/building/round-tower.png")).toExternalForm()), 450, 40, 1.8,
+            10),
     ;
     private final String name;
     private final int cost;
@@ -22,10 +41,11 @@ public enum DefensiveBuildingType {
     private final double rangeRate;
     private final int capacity;
     private final boolean isRepairable;
-    private Image image;
+    private final Image image;
 
-    DefensiveBuildingType(String name, int maxHitpoints, int stoneAmount, double rangeRate, int capacity) {
+    DefensiveBuildingType(String name, Image image, int maxHitpoints, int stoneAmount, double rangeRate, int capacity) {
         this.name = name;
+        this.image = image;
         this.cost = 0;
         this.maxHitpoints = maxHitpoints;
         this.stoneAmount = stoneAmount;
--- src/main/java/model/people/TroopType.java
+++ src/main/java/model/people/TroopType.java
@@ -3,73 +3,72 @@ package model.people;
 import javafx.scene.image.Image;
 import model.buildings.BuildingType;
 import model.game.Item;
-import model.game.Texture;
 
 import java.util.Objects;
 
 public enum TroopType {
 
     //LORD:
-    LORD("", new Image(Objects.requireNonNull(Texture.class.getResource
+    LORD("", new Image(Objects.requireNonNull(TroopType.class.getResource
             ("/Image/troop/lord.png")).toExternalForm()), null, 0, 100,
             100, 55, 1, null, null, false, false),
 
     // Barracks:
-    ARCHER("archer", new Image(Objects.requireNonNull(Texture.class.getResource
+    ARCHER("archer", new Image(Objects.requireNonNull(TroopType.class.getResource
             ("/Image/troop/european/archer.png")).toExternalForm()), BuildingType.BARRACKS, 12, 45,
             40, 90, 10, Item.BOW, null, true, true),
-    SPEARMAN("spearman", new Image(Objects.requireNonNull(Texture.class.getResource
+    SPEARMAN("spearman", new Image(Objects.requireNonNull(TroopType.class.getResource
             ("/Image/troop/european/spearman.png")).toExternalForm()), BuildingType.BARRACKS, 8, 50,
             50, 60, 1, Item.SPEAR, null, true, true),
-    MaceMan("maceman", new Image(Objects.requireNonNull(Texture.class.getResource
+    MaceMan("maceman", new Image(Objects.requireNonNull(TroopType.class.getResource
             ("/Image/troop/european/maceman.png")).toExternalForm()), BuildingType.BARRACKS, 20, 70,
             75, 85, 1, Item.MACE, Item.LEATHER_ARMOR, true, true),
-    CROSSBOWMAN("crossbowman", new Image(Objects.requireNonNull(Texture.class.getResource
+    CROSSBOWMAN("crossbowman", new Image(Objects.requireNonNull(TroopType.class.getResource
             ("/Image/troop/european/crossbowman.png")).toExternalForm()), BuildingType.BARRACKS, 20, 75,
             70, 55, 8, Item.CROSSBOW, Item.LEATHER_ARMOR, false, false),
-    PIKEMAN("pikeman", new Image(Objects.requireNonNull(Texture.class.getResource
+    PIKEMAN("pikeman", new Image(Objects.requireNonNull(TroopType.class.getResource
             ("/Image/troop/european/pikeman.png")).toExternalForm()), BuildingType.BARRACKS, 20, 77,
             76, 60, 1, Item.PIKE, Item.METAL_ARMOR, false, true),
-    SWORDSMAN("swordsman", new Image(Objects.requireNonNull(Texture.class.getResource
+    SWORDSMAN("swordsman", new Image(Objects.requireNonNull(TroopType.class.getResource
             ("/Image/troop/european/swordsman.png")).toExternalForm()), BuildingType.BARRACKS, 40, 94,
             94, 40, 1, Item.SWORD, Item.METAL_ARMOR, false, false),
-    KNIGHT("knight", new Image(Objects.requireNonNull(Texture.class.getResource
+    KNIGHT("knight", new Image(Objects.requireNonNull(TroopType.class.getResource
             ("/Image/troop/european/knight.png")).toExternalForm()), BuildingType.BARRACKS, 40, 90,
             94, 95, 1, Item.SWORD, Item.METAL_ARMOR, Item.HORSE, false, false),
 
     // Mercenary post:
-    ARABIAN_ARCHER("arabian archer", new Image(Objects.requireNonNull(Texture.class.getResource
+    ARABIAN_ARCHER("arabian archer", new Image(Objects.requireNonNull(TroopType.class.getResource
             ("/Image/troop/arabian/archer.png")).toExternalForm()), BuildingType.MERCENARY_POST, 75,
             45, 45, 90, 12, null, null, false, true),
-    SLAVE("slave", new Image(Objects.requireNonNull(Texture.class.getResource
+    SLAVE("slave", new Image(Objects.requireNonNull(TroopType.class.getResource
             ("/Image/troop/arabian/slave.png")).toExternalForm()), BuildingType.MERCENARY_POST, 5,
             20, 10, 90, 1, null, null, false, true),
-    SLINGER("slinger", new Image(Objects.requireNonNull(Texture.class.getResource
+    SLINGER("slinger", new Image(Objects.requireNonNull(TroopType.class.getResource
             ("/Image/troop/arabian/slinger.png")).toExternalForm()), BuildingType.MERCENARY_POST, 12,
             20, 36, 90, 6, null, null, false, false),
-    HORSE_ARCHER("horse archer", new Image(Objects.requireNonNull(Texture.class.getResource
+    HORSE_ARCHER("horse archer", new Image(Objects.requireNonNull(TroopType.class.getResource
             ("/Image/troop/arabian/horse-archer.png")).toExternalForm()), BuildingType.MERCENARY_POST, 80,
             55, 50, 95, 12, null, null, false, false),
-    ARABIAN_SWORDSMAN("arabian swordsman", new Image(Objects.requireNonNull(Texture.class.getResource
+    ARABIAN_SWORDSMAN("arabian swordsman", new Image(Objects.requireNonNull(TroopType.class.getResource
             ("/Image/troop/arabian/swordsman.png")).toExternalForm()), BuildingType.MERCENARY_POST, 80,
             88, 88, 40, 1, null, null, false, false),
-    ASSASSIN("assassin", new Image(Objects.requireNonNull(Texture.class.getResource
+    ASSASSIN("assassin", new Image(Objects.requireNonNull(TroopType.class.getResource
             ("/Image/troop/arabian/assassin.png")).toExternalForm()), BuildingType.MERCENARY_POST, 60,
             73, 76, 67, 1, null, null, false, false),
-    FIRE_THROWER("fire thrower", new Image(Objects.requireNonNull(Texture.class.getResource
+    FIRE_THROWER("fire thrower", new Image(Objects.requireNonNull(TroopType.class.getResource
             ("/Image/troop/arabian/fire-thrower.png")).toExternalForm()), BuildingType.MERCENARY_POST, 100,
             60, 84, 60, 4, null, null, false, false),
 
     // Engineer guild:
-    ENGINEER("engineer", new Image(Objects.requireNonNull(Texture.class.getResource
+    ENGINEER("engineer", new Image(Objects.requireNonNull(TroopType.class.getResource
             ("/Image/troop/european/engineer.png")).toExternalForm()), BuildingType.ENGINEER_GUILD, 30,
             10, 0, 60, 1, null, null, false, false),
-    LADDERMAN("ladderman", new Image(Objects.requireNonNull(Texture.class.getResource
+    LADDERMAN("ladderman", new Image(Objects.requireNonNull(TroopType.class.getResource
             ("/Image/troop/european/ladderman.png")).toExternalForm()), BuildingType.ENGINEER_GUILD, 4,
             5, 0, 60, 1, null, null, false, false),
 
     // Tunneler guild:
-    TUNNELER("tunneler", new Image(Objects.requireNonNull(Texture.class.getResource
+    TUNNELER("tunneler", new Image(Objects.requireNonNull(TroopType.class.getResource
             ("/Image/troop/european/tunneler.png")).toExternalForm()), BuildingType.TUNNELER_GUILD, 30,
             20, 0, 60, 20, null, null, false, false);
 
