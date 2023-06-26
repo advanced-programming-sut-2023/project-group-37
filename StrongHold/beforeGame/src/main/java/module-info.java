dadd2c45ba941b5e2c737443051ecb2e8cb4b664
Payea <mmdPayea@gmail.com>
2023-06-26 15:23:36 +0330

    gradle successfully added ! yessss !:))

---
+++ StrongHold/beforeGame/src/main/java/module-info.java
@@ -0,0 +1,23 @@
+module Stronghold {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires spring.security.crypto;
+    requires com.google.gson;
+
+    exports view;
+    exports model.buildings;
+    exports model.game;
+    exports model.people;
+    exports model.utils;
+
+    exports controller;
+    opens controller to javafx.fxml;
+    exports view.enums;
+    opens view.enums to javafx.fxml;
+    exports view.menus;
+    opens view.menus to javafx.fxml;
+    exports model.user;
+    opens model.user to com.google.gson;
+    exports controller.viewControllers;
+    opens controller.viewControllers to javafx.fxml;
+}
---
+++ StrongHold/core/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new File("").getAbsolutePath());
+    }
+}
