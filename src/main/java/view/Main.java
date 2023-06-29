79ed579d86a37a81a1a5edbe8cd0a07d15da8677
Mehrshad Dehghani <Dehghani.Mehrshad82@gmail.com>
2023-06-29 17:10:39 +0330

    small changes

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -8,6 +8,7 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
     @Override
     public void start(Stage stage) throws Exception {
         new AppController(stage).loadApp();
