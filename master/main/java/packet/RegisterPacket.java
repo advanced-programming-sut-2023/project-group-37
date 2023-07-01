aed5793ccdc07455ee2c8e445a64965cda6f8cdf
Payea <mmdPayea@gmail.com>
2023-07-01 19:19:04 +0330

    master added successfully

--- master/main/java/packet/RegisterPacket.java
+++
@@ -1,7 +0,0 @@
-package packet;
-
-import java.io.Serializable;
-
-public record RegisterPacket(String username, String hashedPassword, String nickname, String slogan, String email,
-                             String recoveryQuestion, String recoveryAnswer) implements Serializable {
-}
