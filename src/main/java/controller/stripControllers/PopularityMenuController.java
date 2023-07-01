266d3490f71301fbf054988960bf23a109c09b2d
Mehrshad Dehghani <Dehghani.Mehrshad82@gmail.com>
2023-07-01 10:40:13 +0330

    on master

--- src/main/java/controller/stripControllers/PopularityMenuController.java
+++ src/main/java/controller/stripControllers/PopularityMenuController.java
@@ -1,6 +1,9 @@
 package controller.stripControllers;
 
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import model.game.Game;
 
 public class PopularityMenuController {
@@ -26,12 +29,36 @@ public class PopularityMenuController {
     }
 
     private void setFearRate() {
+        Label Fear = new Label("Popularity : " + game.getCurrentTurnGovernment().getFearRate());
+
+        Fear.setLayoutX(300);
+        Fear.setLayoutY(15);
+        Fear.setStyle("-fx-font-size: 20");
+        Fear.setBackground(Background.fill(Color.WHITE));
+
+        stripPane.getChildren().add(Fear);
     }
 
     private void setTaxes() {
+        Label tax = new Label("Popularity : " + game.getCurrentTurnGovernment().getTaxRate());
+
+        tax.setLayoutX(200);
+        tax.setLayoutY(15);
+        tax.setStyle("-fx-font-size: 20");
+        tax.setBackground(Background.fill(Color.WHITE));
+
+        stripPane.getChildren().add(tax);
     }
 
     private void setPopularity() {
+        Label popularity = new Label("Popularity : " + game.getCurrentTurnGovernment().getPopularity());
+
+        popularity.setLayoutX(100);
+        popularity.setLayoutY(15);
+        popularity.setStyle("-fx-font-size: 20");
+        popularity.setBackground(Background.fill(Color.WHITE));
+
+        stripPane.getChildren().add(popularity);
     }
 
 }
