d429105397ff64744852b0bb6cd1416fb519101e
Bardia <itsBardiaMandegar@gmail.com>
2023-07-03 14:38:23 +0330

    sync done

--- master/src/main/java/model/chat/Chat.java
+++ master/src/main/java/model/chat/Chat.java
@@ -7,14 +7,28 @@ import java.util.Arrays;
 
 public abstract class Chat {
     private final User owner;
+    private final ChatType type;
     private final ArrayList<User> subscribers;
     private final ArrayList<ChatMessage> messages;
 
-    public Chat(User owner, User... subscribers) {
+    public enum ChatType {
+        PRIVATE, ROOM, PUBLIC
+    }
+
+    public Chat(User owner, ChatType type, User... subscribers) {
         this.owner = owner;
+        this.type = type;
         this.subscribers = new ArrayList<>();
         this.subscribers.addAll(Arrays.asList(subscribers));
         this.messages = new ArrayList<>();
+    }
+
+    public User getOwner() {
+        return this.owner;
+    }
+
+    public ChatType getType() {
+        return this.type;
     }
 
     public ArrayList<User> getSubscribers() {
@@ -23,9 +37,5 @@ public abstract class Chat {
 
     public ArrayList<ChatMessage> getMessages() {
         return this.messages;
-    }
-
-    public User getOwner() {
-        return this.owner;
     }
 }
