77c472939a312ddf1561f0558a716cef7e88c0f6
Bardia <itsBardiaMandegar@gmail.com>
2023-07-03 14:39:22 +0330

    PublicChat bug fixed

--- master/src/main/java/model/chat/PublicChat.java
+++ master/src/main/java/model/chat/PublicChat.java
@@ -7,7 +7,7 @@ public class PublicChat extends Chat {
     private static PublicChat publicChat;
 
     public PublicChat(User owner, User... subscribers) {
-        super(owner, subscribers);
+        super(owner, ChatType.PUBLIC, subscribers);
     }
 
     public static PublicChat getInstance() {
