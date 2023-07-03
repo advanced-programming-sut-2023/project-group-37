31fba28d9107eb38ec93832e9be63d9b45e7b353
Payea <mmdPayea@gmail.com>
2023-07-03 18:53:47 +0330

    requesting create lobby

--- client/src/main/java/connection/packet/relation/RequestLobbyPacket.java
+++ client/src/main/java/connection/packet/relation/RequestLobbyPacket.java
@@ -3,11 +3,11 @@ package connection.packet.relation;
 import connection.packet.Packet;
 import connection.packet.PacketType;
 
-public class LobbyPacket extends Packet {
+public class RequestLobbyPacket extends Packet {
     private final int capacity;
-    public LobbyPacket(int capacity) {
+    public RequestLobbyPacket(int capacity) {
         this.capacity = capacity;
-        super.type = PacketType.LOBBY_PACKET;
+        super.type = PacketType.REQUEST_LOBBY_PACKET;
     }
 
     public int getCapacity() {
