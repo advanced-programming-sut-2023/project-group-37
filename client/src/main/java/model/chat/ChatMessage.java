ac83b4147a480b67f9111974c0dd7969ba1d07ff
Bardia <itsBardiaMandegar@gmail.com>
2023-07-03 15:56:27 +0330

    timeSent and id added

--- client/src/main/java/model/chat/ChatMessage.java
+++ client/src/main/java/model/chat/ChatMessage.java
@@ -1,24 +1,42 @@
 package model.chat;
 
-import javafx.scene.image.Image;
 import model.user.User;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.time.format.DateTimeFormatter;
 
 public class ChatMessage {
+    private static int idCounter;
+    private final int id;
+    private static final DateTimeFormatter formatter;
     private final String senderUsername;
+    private final String timeSent;
     private String message;
     private MessageState state;
 
+    static {
+        idCounter = 0;
+        formatter = DateTimeFormatter.ofPattern("HH:mm");
+    }
+
     public ChatMessage(User sender, String message) {
+        this.id = idCounter++;
         this.senderUsername = sender.getUsername();
+        this.timeSent = LocalDateTime.now().toString();
         this.message = message;
         this.state = MessageState.SENDING;
     }
 
+    public int getId() {
+        return this.id;
+    }
+
     public String getSenderUsername() {
         return this.senderUsername;
+    }
+
+    public String getTimeSent() {
+        return this.timeSent;
     }
 
     public String getMessage() {
@@ -35,5 +53,9 @@ public class ChatMessage {
 
     public void setState(MessageState state) {
         this.state = state;
+    }
+
+    public String getFormattedTimeSent() {
+        return formatter.format(LocalDateTime.parse(this.timeSent));
     }
 }
--- master/src/main/java/model/chat/Chat.java
+++ master/src/main/java/model/chat/Chat.java
@@ -6,21 +6,32 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public abstract class Chat {
+    private static int idCounter;
+    private final int id;
     private final User owner;
     private final ChatType type;
     private final ArrayList<User> subscribers;
     private final ArrayList<ChatMessage> messages;
+
+    static {
+        idCounter = 0;
+    }
 
     public enum ChatType {
         PRIVATE, ROOM, PUBLIC
     }
 
     public Chat(User owner, ChatType type, User... subscribers) {
+        this.id = idCounter++;
         this.owner = owner;
         this.type = type;
         this.subscribers = new ArrayList<>();
         this.subscribers.addAll(Arrays.asList(subscribers));
         this.messages = new ArrayList<>();
+    }
+
+    public int getId() {
+        return this.id;
     }
 
     public User getOwner() {
